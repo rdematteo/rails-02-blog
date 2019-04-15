0ca3e5620554f33b0cbafb4b9e3d41dc881f485a
Robert De Matteo <robertdematteo@hotmail.com>
2019-04-15 20:53:12 +1000

    5.11 Updating Articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,6 +12,11 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
    
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+
   def create
     @article = Article.new(article_params)
    
@@ -22,6 +27,18 @@ class ArticlesController < ApplicationController
     end
   end
    
+  def update
+    @article = Article.find(params[:id])
+   
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
+
+
   private
     def article_params
       params.require(:article).permit(:title, :text)
