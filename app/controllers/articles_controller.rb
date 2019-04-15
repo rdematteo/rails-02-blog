8ec25838509e3f5eee54d7df5919082043d95857
Robert De Matteo <robertdematteo@hotmail.com>
2019-04-15 20:23:32 +1000

    5.7 Showing Articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,14 @@
 class ArticlesController < ApplicationController
+  
+  def show
+    @article = Article.find(params[:id])
+  end
+  
+  
   def new
   end
 
+  
   def create
     @article = Article.new(params.require(:article).permit(:title, :text))
  
